eb71284c16c4268d31ca35d32421eb780ee1565c
Momo-co <suman_grg@outlook.com>
2022-07-06 17:24:55 +0100

    Initial Commit

---
+++ VideoApp/VideoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoAppApp.swift
+//  VideoApp
+//
+//  Created by Suman Gurung on 06/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct VideoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
